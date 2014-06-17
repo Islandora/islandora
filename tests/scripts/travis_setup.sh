44b92b4b2b6cabee890373bfd867786123628305
qadan <daitken@discoverygarden.ca>
2014-06-17 13:16:43 -0300

    fix travis logging

--- tests/scripts/travis_setup.sh
+++ tests/scripts/travis_setup.sh
@@ -8,6 +8,7 @@ cd $HOME
 git clone git://github.com/Islandora/tuque.git
 git clone -b $FEDORA_VERSION git://github.com/Islandora/islandora_tomcat.git
 cd islandora_tomcat
+git fsck
 export CATALINA_HOME='.'
 export JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=512m -XX:+CMSClassUnloadingEnabled -Djavax.net.ssl.trustStore=$CATALINA_HOME/fedora/server/truststore -Djavax.net.ssl.trustStorePassword=tomcat"
 ./bin/startup.sh
@@ -18,10 +19,18 @@ pear upgrade --force pear
 pear channel-discover pear.drush.org
 
 # "prefer-source" required due to SSL shenanigans on the Travis boxes...
-composer global require --prefer-source 'squizlabs/php_codesniffer=*' 'sebastian/phpcpd=*' 'drush/drush:6.3.0'
+composer global require --prefer-source 'squizlabs/php_codesniffer=*' 'sebastian/phpcpd=*'
 # Because we can't add to the PATH here and this file is used in many repos,
 # let's just throw symlinks in.
 find $HOME/.composer/vendor/bin -executable \! -type d -exec sudo ln -s {}  /usr/local/sbin/ \;
+
+# Install Drush
+git clone https://github.com/drush-ops/drush.git
+pushd drush
+git checkout 5.9.0
+chmod +x drush
+popd
+sudo ln -s $HOME/drush/drush /usr/local/sbin
 
 phpenv rehash
 drush dl --yes drupal
@@ -31,7 +40,7 @@ drush si minimal --db-url=mysql://drupal:drupal@localhost/drupal --yes
 sudo chmod a+w sites/default/settings.php
 echo "include_once '$HOME/.composer/vendor/autoload.php';" >> sites/default/settings.php
 sudo chmod a-w sites/default/settings.php
-drush runserver --php-cgi=$HOME/.phpenv/shims/php-cgi localhost:8081 &>/dev/null &
+drush runserver --php-cgi=$HOME/.phpenv/shims/php-cgi localhost:8081 &>/tmp/drush_webserver.log &
 ln -s $ISLANDORA_DIR sites/all/modules/islandora
 mv sites/all/modules/islandora/tests/travis.test_config.ini sites/all/modules/islandora/tests/test_config.ini
 mkdir sites/all/libraries
