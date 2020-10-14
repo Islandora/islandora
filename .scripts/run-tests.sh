18703e9c6382365e1cb4d1aca8202fa870050da4
Jared Whiklo <jwhiklo@gmail.com>
2020-10-14 13:55:06 -0500

    Have to use Drupal types not classes/interfaces

--- .scripts/run-tests.sh
+++ .scripts/run-tests.sh
@@ -3,8 +3,8 @@
 # Wrapper for the test executing function so we only have to change it in one place.
 # The module name gets passed in as a command line arg.
 if [ "$1" == "unit" ]; then
-  TYPE="--types \"IslandoraKernelTestBase\""
+  TYPE="--types \"PHPUnit-Unit,PHPUnit-Kernel,Simpletest\""
 elif [ "$1" == "functional" ]; then
-  TYPE="--types \"IslandoraFunctionalTestBase,WebDriverTestBase\""
+  TYPE="--types \"PHPUnit-FunctionalJavascript,PHPUnit-Functional\""
 fi
 php core/scripts/run-tests.sh --suppress-deprecations --concurrency 2 --url http://127.0.0.1:8282 --verbose --php `which php` --module "$2" $TYPE
