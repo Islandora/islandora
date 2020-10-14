bb348cc343d611099ba6769c9de518666b1be54c
Jared Whiklo <jwhiklo@gmail.com>
2020-10-14 13:42:53 -0500

    fix types variable

--- .scripts/run-tests.sh
+++ .scripts/run-tests.sh
@@ -3,8 +3,8 @@
 # Wrapper for the test executing function so we only have to change it in one place.
 # The module name gets passed in as a command line arg.
 if [ "$1" == "unit" ]; then
-  TYPE="--type=IslandoraKernelTestBase"
+  TYPE="--types \"IslandoraKernelTestBase\""
 elif [ "$1" == "functional" ]; then
-  TYPE="--type=IslandoraFunctionalTestBase,WebDriverTestBase"
+  TYPE="--types \"IslandoraFunctionalTestBase,WebDriverTestBase\""
 fi
 php core/scripts/run-tests.sh --suppress-deprecations --concurrency 2 --url http://127.0.0.1:8282 --verbose --php `which php` --module "$2" $TYPE
