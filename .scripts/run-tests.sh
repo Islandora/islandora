2a346edb3b76a1826eefbdeb451a9090ed4c4f85
Jared Whiklo <jwhiklo@gmail.com>
2020-10-15 12:50:09 -0500

    Display run-tests command line

--- .scripts/run-tests.sh
+++ .scripts/run-tests.sh
@@ -7,4 +7,6 @@ if [ "$1" == "unit" ]; then
 elif [ "$1" == "functional" ]; then
   TYPE="--types \"PHPUnit-FunctionalJavascript,PHPUnit-Functional\""
 fi
-php core/scripts/run-tests.sh --suppress-deprecations --concurrency 2 --url http://127.0.0.1:8282 --verbose --php `which php` --module "$2" $TYPE
+COMMAND="php core/scripts/run-tests.sh --suppress-deprecations --concurrency 2 --url http://127.0.0.1:8282 --verbose --php `which php` $TYPE --module \"$2\""
+echo $COMMAND
+$COMMAND
