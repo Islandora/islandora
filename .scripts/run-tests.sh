5850fe956e3c004ddd3da8f0f636679d562ff373
Jared Whiklo <jwhiklo@gmail.com>
2020-10-16 15:24:05 -0500

    Make directory and use correct testsuite

--- .scripts/run-tests.sh
+++ .scripts/run-tests.sh
@@ -2,9 +2,6 @@
 
 # Wrapper for the test executing function so we only have to change it in one place.
 # The module name gets passed in as a command line arg.
-LS="ls ../modules/contrib/islandora/tests/src/Kernel"
-echo "LS Command -> $LS"
-$LS
 COMMAND="$DRUPAL_DIR/vendor/bin/phpunit --verbose --testsuite $1"
 echo $COMMAND
 $COMMAND
