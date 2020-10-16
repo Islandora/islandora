704689cd1bc15e8f0ce88a30f2244dfdd57abe96
Jared Whiklo <jwhiklo@gmail.com>
2020-10-16 12:49:25 -0500

    Try using phpunit

--- .scripts/run-tests.sh
+++ .scripts/run-tests.sh
@@ -2,11 +2,6 @@
 
 # Wrapper for the test executing function so we only have to change it in one place.
 # The module name gets passed in as a command line arg.
-if [ "$1" == "unit" ]; then
-  TYPE="--types \"PHPUnit-Unit,PHPUnit-Kernel,Simpletest\""
-elif [ "$1" == "functional" ]; then
-  TYPE="--types \"PHPUnit-FunctionalJavascript,PHPUnit-Functional\""
-fi
-COMMAND="php core/scripts/run-tests.sh --suppress-deprecations --concurrency 2 --url http://127.0.0.1:8282 --verbose --php `which php` $TYPE --module \"$2\""
+COMMAND="$DRUPAL_DIR/vendor/bin/phpunit -c $PHPUNIT_FILE --verbose --testsuite "$1"
 echo $COMMAND
 $COMMAND
