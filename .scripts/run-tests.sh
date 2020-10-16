0fd579bf7aa5f87930036545460a85c1a277e667
Jared Whiklo <jwhiklo@gmail.com>
2020-10-16 13:54:43 -0500

    Need to be at drupal core

--- .scripts/run-tests.sh
+++ .scripts/run-tests.sh
@@ -2,6 +2,6 @@
 
 # Wrapper for the test executing function so we only have to change it in one place.
 # The module name gets passed in as a command line arg.
-COMMAND="$DRUPAL_DIR/vendor/bin/phpunit -c $PHPUNIT_FILE --verbose --testsuite $1"
+COMMAND="$DRUPAL_DIR/vendor/bin/phpunit --verbose --testsuite $1"
 echo $COMMAND
 $COMMAND
