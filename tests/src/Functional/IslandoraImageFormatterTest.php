35e4e895ffb7895ebda1cc1d1b7368431091f514
Rosie Le Faive <lefaive@gmail.com>
2022-05-17 13:59:05 -0300

    Coding standards.

--- tests/src/Functional/IslandoraImageFormatterTest.php
+++ tests/src/Functional/IslandoraImageFormatterTest.php
@@ -32,7 +32,11 @@ class IslandoraImageFormatterTest extends IslandoraFunctionalTestBase {
     // Also, only show the image on display to remove clutter.
     $display_options = [
       'type' => 'islandora_image',
-      'settings' => ['image_style' => NULL, 'image_link' => 'content', 'image_loading' => 'lazy'],
+      'settings' => [
+        'image_style' => NULL,
+        'image_link' => 'content',
+        'image_loading' => 'lazy',
+      ],
     ];
     $display = $this->container->get('entity_display.repository')->getViewDisplay('media', $testImageMediaType->id(), 'default');
     $display->setComponent('field_media_image', $display_options)
