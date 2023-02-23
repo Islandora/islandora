c1ace5a2da58592b91dce7c83b97e1f4a693b5d7
Don Richards <2738244+DonRichards@users.noreply.github.com>
2023-02-23 16:13:56 -0500

    Replace the watchdog output

--- src/MediaSource/MediaSourceService.php
+++ src/MediaSource/MediaSourceService.php
@@ -295,7 +295,7 @@ class MediaSourceService {
         $error_current_user = shell_exec('whoami');
         $sanitized_current_user = str_replace(array("\n", "\r"), '', $error_current_user);
         $error_message = "The destination directory does not exist, could not be created, or is not writable by $sanitized_current_user: $directory";
-        watchdog('media_source_service', $error_message, [], WATCHDOG_ERROR);
+        $this->logger->error($error_message);
         throw new HttpException(500, "An error occurred while processing your request. Please contact the website administrator.");
       }
 
