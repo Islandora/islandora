bb525ebe38c19a50105beddcf7da9e7c030f06bc
dannylamb <dlamb@islandora.ca>
2023-08-24 15:36:53 -0300

    Stripping out json metadata in the queue messages except for the ones java is expecting

--- src/EventGenerator/EventGenerator.php
+++ src/EventGenerator/EventGenerator.php
@@ -147,8 +147,11 @@ class EventGenerator implements EventGeneratorInterface {
       }
     }
 
-    unset($data["event"]);
-    unset($data["queue"]);
+    $allowed_keys = ["file_upload_uri", "fedora_uri", "source_uri", "destination_uri", "args", "mimetype", "source_field"];
+    $keys_to_unset = array_diff(array_keys($data), $allowed_keys);
+    foreach ($keys_to_unset as $key) {
+      unset($data[$key]);
+    }
 
     if (!empty($data)) {
       $event["attachment"] = [
