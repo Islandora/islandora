17fe2879841c141cd1caa227aedf810e9142335e
Joe Corall <joe@libops.io>
2023-11-03 14:27:56 -0400

    lint (#994)
    * lint
    
    * Update JsonldTypeAlterReactionTest.php

--- tests/src/Functional/JsonldTypeAlterReactionTest.php
+++ tests/src/Functional/JsonldTypeAlterReactionTest.php
@@ -36,20 +36,21 @@ class JsonldTypeAlterReactionTest extends JsonldSelfReferenceReactionTest {
     }
     else {
       $this->getSession()->getPage()->selectFieldOption('new_storage_type', 'plain_text');
-      // first need to submit the form with the elements displayed on initial page load
-      // the form is using AJAX to send a second element after we selected the radio button above
-      // we can instead get the second element by submitting the form and having it throw an error
-      // since the required field is missing
-      // @TODO: refactor this as a functional javascript test maybe?
-      // though we currently don't have selenium running for our tests so not possible ATM
+      // First need to submit the form with the elements displayed
+      // on initial page load. The form is using AJAX to send a second element
+      // after we selected the radio button above
+      // we can instead get the second element by submitting the form
+      // and having it throw an error since the required field is missing.
+      // @todo refactor this as a functional javascript test.
       $this->submitForm([
         'new_storage_type' => 'plain_text',
         'label' => 'Typed Predicate',
         'field_name' => 'type_predicate',
       ], $this->t('Continue'));
 
-      // now we can proceed, selecting the plain text (i.e. string) for the second element
-      // now that the element is displayed after the initial form submission
+      // Now we can proceed, selecting the plain text (i.e. string)
+      // for the second element now that the element is displayed after
+      // the initial form submission.
       $this->getSession()->getPage()->selectFieldOption('group_field_options_wrapper', 'string');
       $this->submitForm([
         'new_storage_type' => 'plain_text',
