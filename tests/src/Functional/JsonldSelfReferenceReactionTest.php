1e6c6f2bd5fbae930682989475d3f45a3ce252c9
Rosie Le Faive <lefaive@gmail.com>
2023-11-08 15:25:28 -0400

    Declare properties so they aren't dynamic.

--- tests/src/Functional/JsonldSelfReferenceReactionTest.php
+++ tests/src/Functional/JsonldSelfReferenceReactionTest.php
@@ -12,6 +12,13 @@ use function GuzzleHttp\json_decode;
  */
 class JsonldSelfReferenceReactionTest extends IslandoraFunctionalTestBase {
 
+	/**
+	 * An RDF Mapping object.
+	 *
+	 * @var \Drupal\rdf\Entity\RdfMapping
+	 */
+	protected $rdfMapping;
+
   /**
    * {@inheritdoc}
    */
