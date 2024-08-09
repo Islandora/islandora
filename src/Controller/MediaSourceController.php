49d4d9dd00a458c5834f26016a6fd7a96be23734
Rosie Le Faive <lefaive@gmail.com>
2024-08-09 16:32:51 -0300

    Use vocabulary's current name, Media Use

--- src/Controller/MediaSourceController.php
+++ src/Controller/MediaSourceController.php
@@ -136,7 +136,7 @@ class MediaSourceController extends ControllerBase {
    * @param \Drupal\media\MediaTypeInterface $media_type
    *   Media type for new media.
    * @param \Drupal\taxonomy\TermInterface $taxonomy_term
-   *   Term from the 'Behavior' vocabulary to give to new media.
+   *   Term from the 'Media Use' vocabulary to give to new media.
    * @param \Symfony\Component\HttpFoundation\Request $request
    *   The request object.
    *
@@ -164,7 +164,7 @@ class MediaSourceController extends ControllerBase {
     $transaction = $this->database->startTransaction();
 
     try {
-      $media = $this->service->putToNode(
+    $media = $this->service->putToNode(
         $node,
         $media_type,
         $taxonomy_term,
