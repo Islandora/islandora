98be651e4f0d5297d8655285fef4c4e408d3201b
joshdentremont <joshdentremont@users.noreply.github.com>
2026-07-23 12:28:41 -0300

    Drupal 11.4 compatibility for IslandoraImageFormatter (#1110)

--- src/Plugin/Field/FieldFormatter/IslandoraImageFormatter.php
+++ src/Plugin/Field/FieldFormatter/IslandoraImageFormatter.php
@@ -2,17 +2,11 @@
 
 namespace Drupal\islandora\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Field\Attribute\FieldFormatter;
-use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\File\FileUrlGeneratorInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
-use Drupal\islandora\IslandoraUtils;
-use Drupal\islandora\MediaSource\MediaSourceService;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -40,81 +34,13 @@ class IslandoraImageFormatter extends ImageFormatter {
   protected $mediaSourceService;
 
   /**
-   * Constructs an IslandoraImageFormatter object.
-   *
-   * @param string $plugin_id
-   *   The plugin_id for the formatter.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
-   *   The definition of the field to which the formatter is associated.
-   * @param array $settings
-   *   The formatter settings.
-   * @param string $label
-   *   The formatter label display setting.
-   * @param string $view_mode
-   *   The view mode.
-   * @param array $third_party_settings
-   *   Any third party settings settings.
-   * @param \Drupal\Core\Session\AccountInterface $current_user
-   *   The current user.
-   * @param \Drupal\Core\Entity\EntityStorageInterface $image_style_storage
-   *   The image style storage.
-   * @param \Drupal\islandora\IslandoraUtils $utils
-   *   Islandora utils.
-   * @param \Drupal\Core\File\FileUrlGeneratorInterface $file_url_generator
-   *   The File URL Generator.
-   * @param \Drupal\islandora\MediaSource\MediaSourceService $media_source_service
-   *   Utils to get the source file from media.
-   */
-  public function __construct(
-    $plugin_id,
-    $plugin_definition,
-    FieldDefinitionInterface $field_definition,
-    array $settings,
-    $label,
-    $view_mode,
-    array $third_party_settings,
-    AccountInterface $current_user,
-    EntityStorageInterface $image_style_storage,
-    IslandoraUtils $utils,
-    FileUrlGeneratorInterface $file_url_generator,
-    MediaSourceService $media_source_service,
-  ) {
-    parent::__construct(
-      $plugin_id,
-      $plugin_definition,
-      $field_definition,
-      $settings,
-      $label,
-      $view_mode,
-      $third_party_settings,
-      $current_user,
-      $image_style_storage,
-      $file_url_generator
-    );
-    $this->utils = $utils;
-    $this->mediaSourceService = $media_source_service;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['label'],
-      $configuration['view_mode'],
-      $configuration['third_party_settings'],
-      $container->get('current_user'),
-      $container->get('entity_type.manager')->getStorage('image_style'),
-      $container->get('islandora.utils'),
-      $container->get('file_url_generator'),
-      $container->get('islandora.media_source_service')
-    );
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->utils = $container->get('islandora.utils');
+    $instance->mediaSourceService = $container->get('islandora.media_source_service');
+    return $instance;
   }
 
   /**
